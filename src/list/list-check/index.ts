e01e2842512aceb5b1f7bc40ace73964b9289c41
洲泛 <zhoufan.hzf@antfin.com>
2019-11-27 12:17:28 +0800

    some fix + list-check waiting for icon

---
+++ src/list/list-check/index.ts
@@ -0,0 +1,42 @@
+Component({
+  props: {
+    className: '',
+    align: false,
+    disabled: false,
+    multipleLine: false,
+    wrap: false,
+    type: 'normal', // 'normal' | 'capsule' | 'primary'
+  },
+  data: {
+    typeClassMap: {
+      normal: 'am-list-extra',
+      primary: 'am-list-primary',
+      capsule: 'am-list-capsule',
+    },
+  },
+  didMount() {
+    this._updateDataSet();
+  },
+  didUpdate() {
+    this._updateDataSet();
+  },
+  methods: {
+    _updateDataSet() {
+      this.dataset = {};
+      for (const key in this.props) {
+        if ((/data-/gi).test(key)) {
+          this.dataset[key.replace(/data-/gi, '')] = this.props[key];
+        }
+      }
+    },
+    onItemTap(ev) {
+      const { onClick, disabled } = this.props;
+      if (onClick && !disabled) {
+        onClick({
+          index: ev.target.dataset.index,
+          target: { dataset: this.dataset },
+        });
+      }
+    },
+  },
+});
