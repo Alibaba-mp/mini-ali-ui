eb5b402b88ed8adcf1ca8df6c565193598bb0838
小志 <yousai.lys@antfin.com>
2019-12-09 11:06:17 +0800

    tabs demo modify

--- demo/pages/tabs/index.js
+++ demo/pages/tabs/index.js
@@ -23,6 +23,63 @@ Page({
       { title: '15 Tab' },
     ],
     activeTab: 0,
+    type: [
+      { name: 'normal', value: '普通', checked: true },
+      { name: 'capsule', value: '胶囊' },
+      { name: 'hasSubTitle', value: '带描述' },
+    ],
+    typeCapsule: false,
+    typeHasSubTitle: false,
+    plus: [
+      { name: 'has', value: '是', checked: true },
+      { name: 'hasnt', value: '否' },
+    ],
+    hasPlus: true,
+    contentHeight: [
+      { name: 'has', value: '是', checked: true },
+      { name: 'hasnt', value: '否' },
+    ],
+    hasContentHeight: true,
+  },
+  typeChange(e) {
+    if (e.detail.value === 'hasSubTitle') {
+      this.setData({
+        typeCapsule: true,
+        typeHasSubTitle: true,
+      });
+    } else if (e.detail.value === 'capsule') {
+      this.setData({
+        typeCapsule: true,
+        typeHasSubTitle: false,
+      });
+    } else {
+      this.setData({
+        typeCapsule: false,
+        typeHasSubTitle: false,
+      });
+    }
+  },
+  plusChange(e) {
+    if (e.detail.value === 'hasnt') {
+      this.setData({
+        hasPlus: false,
+      });
+    } else {
+      this.setData({
+        hasPlus: true,
+      });
+    }
+  },
+  heightChange(e) {
+    if (e.detail.value === 'hasnt') {
+      this.setData({
+        hasContentHeight: false,
+      });
+    } else {
+      this.setData({
+        hasContentHeight: true,
+      });
+    }
   },
   handleTabClick({ index }) {
     this.setData({
