ba2cd4f082a5cdc6e8deb329a747bdc3cef8cdb0
洲泛 <zhoufan.hzf@antfin.com>
2019-12-13 18:12:37 +0800

    support toggle expand/collapse

--- demo/pages/card/index.js
+++ demo/pages/card/index.js
@@ -2,6 +2,7 @@ Page({
   data: {
     thumb: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
     footerImg: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+    expand3rd: false,
   },
   onCardClick(ev) {
     my.alert({
@@ -16,6 +17,11 @@ Page({
   onExtraActionClick() {
     my.alert({
       content: 'extra action clicked',
+    });
+  },
+  toggle() {
+    this.setData({
+      expand3rd: !this.data.expand3rd,
     });
   },
 });
