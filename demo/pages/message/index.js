bdab24a87d7c87f3b1e615eb5373bb2012e9324c
henxi.wf <henxi.wf@alibaba-inc.com>
2019-12-26 11:15:47 +0800

    add message

--- demo/pages/message/index.js
+++ demo/pages/message/index.js
@@ -1,1 +1,51 @@
-Page({});
+Page({
+  data: {
+    title: '操作成功',
+    subTitle: '内容详情可折行，建议不超过两内容详情可折行，建议不超过两行',
+    type: 'success',
+    items: [
+      { name: 'success', value: 'success', checked: true },
+      { name: 'fail', value: 'fail' },
+      { name: 'info', value: 'info' },
+      { name: 'warn', value: 'warn' },
+      { name: 'waiting', value: 'waiting' },
+    ],
+  },
+  onLoad() {
+  },
+  goBack() {
+    my.navigateBack();
+  },
+  radioChange(e) {
+    this.setData({
+      type: e.detail.value,
+    });
+  },
+  titleChange(e) {
+    this.setData({
+      title: e.detail.value,
+    });
+  },
+  subtitleChange(e) {
+    this.setData({
+      subTitle: e.detail.value,
+    });
+  },
+  onChange(e) {
+    if (e.detail.value) {
+      this.setData({
+        mainButton: {
+          buttonText: '主要操作',
+        },
+        subButton: {
+          buttonText: '辅助操作',
+        },
+      });
+    } else {
+      this.setData({
+        mainButton: null,
+        subButton: null,
+      });
+    }
+  },
+});
--- src/message/index.ts
+++ src/message/index.ts
@@ -1,3 +1,12 @@
+const SUPPORT_COMPONENT2 = my.canIUse('component2');
+const TYPE_MAP = {
+  success: 'success_',
+  fail: 'del_',
+  info: 'help1_',
+  warn: 'warn_',
+  waiting: 'time_',
+};
+
 Component({
   props: {
     className: '',
@@ -6,12 +15,37 @@ Component({
     onTapMain: () => {},
     onTapSub: () => {},
   },
+  data: {
+    iconType: 'success_',
+  },
+  onInit() {
+    this.setType(this.props.type);
+  },
+  didMount() {
+    if (!SUPPORT_COMPONENT2) {
+      this.setType(this.props.type);
+    }
+  },
+  didUpdate(prevProps) {
+    if (!SUPPORT_COMPONENT2 && this.props.type !== prevProps.type) {
+      this.setType(this.props.type);
+    }
+  },
+  deriveDataFromProps(nextProps) {
+    if (this.props.type !== nextProps.type) {
+      this.setType(nextProps.type);
+    }
+  },
   methods: {
     tapMain() {
       this.props.onTapMain();
     },
     tapSub() {
       this.props.onTapSub();
+    },
+    setType(type) {
+      const realType = TYPE_MAP[type] || 'success_';
+      this.setData({ iconType: realType });
     },
   },
 });
