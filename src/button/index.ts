296271bb42e176d14a73c16bc372b07ace78ec4d
henxi.wf <henxi.wf@alibaba-inc.com>
2019-12-30 16:33:12 +0800

    change demo

--- src/button/index.ts
+++ src/button/index.ts
@@ -1,5 +1,6 @@
 import fmtClass from '../_util/fmtClass';
 
+const SUPPORT_COMPONENT2 = my.canIUse('component2');
 const prefixCls = 'am-button';
 const noop = () => {};
 Component({
@@ -13,27 +14,53 @@ Component({
     disabled: false,
     subtitle: '',
     onTap: noop,
+    capsuleMinWidth: false,
   },
-  didMount() {
+  onInit() {
     if (!this.props.hoverClass) {
       this.props.hoverClass = 'am-button-active';
       if (this.props.type === 'text') {
         this.props.hoverClass = 'am-button-active-text';
       }
     }
-    this.setData({ baseClass: this.wrapBaseCls(), ...this.props });
+    this.setData({ baseClass: this.wrapBaseCls(this.props) });
   },
-  didUpdate() {},
+  deriveDataFromProps(nextProps) {
+    if (this.propsChange(this.props, nextProps)) {
+      this.setData({ baseClass: this.wrapBaseCls(nextProps) });
+    }
+  },
+  didMount() {
+    if (!SUPPORT_COMPONENT2) {
+      if (!this.props.hoverClass) {
+        this.props.hoverClass = 'am-button-active';
+        if (this.props.type === 'text') {
+          this.props.hoverClass = 'am-button-active-text';
+        }
+      }
+      this.setData({ baseClass: this.wrapBaseCls(this.props) });
+    }
+  },
+  didUpdate(prevProps) {
+    if (!SUPPORT_COMPONENT2 && this.propsChange(prevProps, this.props)) {
+      this.setData({ baseClass: this.wrapBaseCls(this.props) });
+    }
+  },
   didUnmount() {},
   methods: {
-    wrapBaseCls() {
+    wrapBaseCls(props) {
       const {
         type,
         disabled,
         subtitle,
         shape,
         capsuleSize = 'medium',
-      } = this.props;
+        capsuleMinWidth,
+      } = props;
+      let capsuleMinWidthCls = '';
+      if (capsuleMinWidth) {
+        capsuleMinWidthCls = `${prefixCls}-capsule-${capsuleSize}-minwidth`;
+      }
 
       const ret = fmtClass({
         [`${prefixCls}`]: true,
@@ -43,7 +70,7 @@ Component({
         [`${prefixCls}-warn-ghost`]: type === 'warn-ghost',
         [`${prefixCls}-text`]: type === 'text',
         [`${prefixCls}-light`]: type === 'light',
-        [`${prefixCls}-capsule ${prefixCls}-capsule-${capsuleSize}`]:
+        [`${prefixCls}-capsule ${prefixCls}-capsule-${capsuleSize} ${capsuleMinWidthCls}`]:
           shape === 'capsule',
         [`${prefixCls}-disabled`]: disabled,
         [`${prefixCls}-subtitle`]: subtitle,
@@ -52,6 +79,20 @@ Component({
     },
     onButtonTap(...args) {
       this.props.onTap(...args);
+    },
+    propsChange(prevProps, nextProps) {
+      const pProps = Object.getOwnPropertyNames(prevProps);
+      const nProps = Object.getOwnPropertyNames(nextProps);
+      if (pProps.length !== nProps.length) {
+        return true;
+      }
+      for (let i = 0; i < pProps.length; i++) {
+        const propName = pProps[i];
+        if (prevProps[propName] !== nextProps[propName]) {
+          return true;
+        }
+      }
+      return false;
     },
   },
 });
