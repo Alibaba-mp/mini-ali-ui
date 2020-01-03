8ea15cc82a62c51045f0f5876d683ebc68fb366b
henxi.wf <henxi.wf@alibaba-inc.com>
2020-01-03 11:06:16 +0800

    add search bar

--- demo/pages/am-checkbox/index.js
+++ demo/pages/am-checkbox/index.js
@@ -6,12 +6,6 @@ Page({
       { checked: true, disabled: true, value: 'c', title: '复选框-默认选中disabled', id: 'checkbox3' },
       { checked: false, disabled: true, value: 'd', title: '复选框-默认未选中disabled', id: 'checkbox4' },
     ],
-    items2: [
-      { name: 'react', value: 'React', checked: true },
-      { name: 'vue', value: 'Vue.js' },
-      { name: 'ember', value: 'Ember.js' },
-      { name: 'backbone', value: 'Backbone.js', disabled: true },
-    ],
   },
   onSubmit(e) {
     my.alert({
