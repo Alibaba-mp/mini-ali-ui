6459f66171a622f988aec3230c0a332f58974643
小志 <yousai.lys@antfin.com>
2020-01-19 13:40:42 +0800

    demo 引导页调整,以及 demo 中 mini-antui 名称调整为mini-ali-ui

--- demo/pages/list/app.js
+++ demo/pages/list/app.js
@@ -1,226 +1,372 @@
 Page({
   data: {
-    components: [
+    componentList: [
       {
-        name: 'List',
-        nameZN: '列表',
-        path: '/pages/new-list/index',
+        components: [
+          {
+            type: '布局导航',
+            list: [
+              {
+                name: 'Container',
+                nameZN: '容器',
+                path: '/pages/container/index',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'Title',
+                nameZN: '模块标题',
+                path: '/pages/title/index',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'List',
+                nameZN: '列表',
+                path: '/pages/list/index',
+              },
+              {
+                name: 'List-item',
+                nameZN: '列表元素',
+                path: '/pages/new-list/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Tabs',
+                nameZN: 'Tabs',
+                path: '/pages/tabs/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'VTabs',
+                nameZN: 'VTabs',
+                path: '/pages/vtabs/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Tabs elevator',
+                nameZN: 'Tabs 电梯组件',
+                path: '/pages/tabs/elevator/elevator',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Card',
+                nameZN: '卡券 & 卡片',
+                path: '/pages/card/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Grid',
+                nameZN: '宫格',
+                path: '/pages/grid/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Steps',
+                nameZN: '步骤进度条',
+                path: '/pages/steps/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Footer',
+                nameZN: '页脚',
+                path: '/pages/footer/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Terms',
+                nameZN: '协议',
+                path: '/pages/terms/index',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'Flex',
+                nameZN: 'Flex 布局',
+                path: '/pages/flex/index',
+              },
+              {
+                name: 'Collapse',
+                nameZN: '折叠面板',
+                path: '/pages/collapse/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+            ],
+          },
+        ],
       },
       {
-        name: 'AmountInput',
-        nameZN: '金额输入',
-        path: '/pages/amount-input/index',
+        components: [
+          {
+            type: '操作浮层',
+            list: [
+              {
+                name: 'Popover',
+                nameZN: '气泡菜单',
+                path: '/pages/popover/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Filter',
+                nameZN: '筛选',
+                path: '/pages/filter/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Modal',
+                nameZN: 'Modal',
+                path: '/pages/modal/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'PopUp',
+                nameZN: '弹出菜单',
+                path: '/pages/popup/index',
+              },
+            ],
+          },
+        ],
       },
       {
-        name: 'AmIcon',
-        nameZN: '图标',
-        path: '/pages/am-icon/index',
+        components: [
+          {
+            type: '结果类',
+            list: [
+              {
+                name: 'PageResult',
+                nameZN: '结果页',
+                path: '/pages/page-result/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Message',
+                nameZN: '信息状态、操作结果',
+                path: '/pages/message/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+            ],
+          },
+        ],
       },
       {
-        name: 'AmSwitch',
-        nameZN: '开关',
-        path: '/pages/am-switch/index',
+        components: [
+          {
+            type: '提示引导',
+            list: [
+              {
+                name: 'Tips',
+                nameZN: '小提示、向导提示',
+                path: '/pages/tips/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Notice',
+                nameZN: '公告',
+                path: '/pages/notice/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Badge',
+                nameZN: '红点，徽标',
+                path: '/pages/badge/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Tag',
+                nameZN: ' 标签',
+                path: '/pages/tag/index',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'Mask',
+                nameZN: '背景蒙层',
+                path: '/pages/mask/mask',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'Guide',
+                nameZN: '遮罩引导',
+                path: '/pages/guide/guide',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'Avatar',
+                nameZN: '头像',
+                path: '/pages/avatar/index',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+            ],
+          },
+        ],
       },
       {
-        name: 'Alphabet',
-        nameZN: '字母检索表',
-        path: '/pages/alphabet/index',
+        components: [
+          {
+            type: '表单类',
+            list: [
+              {
+                name: 'Form',
+                nameZN: '表单',
+                path: '/pages/form/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'AmountInput',
+                nameZN: '金额输入',
+                path: '/pages/amount-input/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Button',
+                nameZN: '按钮',
+                path: '/pages/button/index',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'AmSwitch',
+                nameZN: '开关',
+                path: '/pages/am-switch/index',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'AMCheckBox',
+                nameZN: '复选框',
+                path: '/pages/am-checkbox/index',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'AMRadio',
+                nameZN: '单选框',
+                path: '/pages/am-radio/index',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'SearchBar',
+                nameZN: '搜索框',
+                path: '/pages/search-bar/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+            ],
+          },
+        ],
       },
       {
-        name: 'Badge',
-        nameZN: '红点',
-        path: '/pages/badge/index',
+        components: [
+          {
+            type: '手势类',
+            list: [
+              {
+                name: 'SwipeAction',
+                nameZN: '滑动删除',
+                path: '/pages/swipe-action/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+            ],
+          },
+        ],
       },
       {
-        name: 'Button',
-        nameZN: '按钮',
-        path: '/pages/button/index',
-      },
-      {
-        name: 'Footer',
-        nameZN: '页脚',
-        path: '/pages/footer/index',
-      },
-      {
-        name: 'Modal',
-        nameZN: 'Modal',
-        path: '/pages/modal/index',
-      },
-      {
-        name: 'Tabs',
-        nameZN: 'Tabs',
-        path: '/pages/tabs/index',
-      },
-      {
-        name: 'VTabs',
-        nameZN: 'VTabs',
-        path: '/pages/vtabs/index',
-      },
-      {
-        name: 'Terms',
-        nameZN: 'Terms',
-        path: '/pages/terms/index',
-      },
-      {
-        name: 'Tabs elevator',
-        nameZN: 'Tabs 电梯组件',
-        path: '/pages/tabs/elevator/elevator',
-      },
-      {
-        name: 'Steps',
-        nameZN: '步骤进度条',
-        path: '/pages/steps/index',
-      },
-      {
-        name: 'Popover',
-        nameZN: '气泡菜单',
-        path: '/pages/popover/index',
-      },
-      {
-        name: 'Grid',
-        nameZN: '宫格',
-        path: '/pages/grid/index',
-      },
-      {
-        name: 'Card',
-        nameZN: '卡券 & 卡片',
-        path: '/pages/card/index',
-      },
-      {
-        name: 'AMCheckBox',
-        nameZN: '复选框',
-        path: '/pages/am-checkbox/index',
-      },
-      {
-        name: 'AMRadio',
-        nameZN: '单选框',
-        path: '/pages/am-radio/index',
-      },
-      {
-        name: 'Filter',
-        nameZN: '筛选',
-        path: '/pages/filter/index',
-      },
-      {
-        name: 'Notice',
-        nameZN: '公告',
-        path: '/pages/notice/index',
-      },
-      {
-        name: 'SearchBar',
-        nameZN: '搜索框',
-        path: '/pages/search-bar/index',
-      },
-      {
-        name: 'Tips',
-        nameZN: '小提示、向导提示',
-        path: '/pages/tips/index',
-      },
-      {
-        name: 'PageResult',
-        nameZN: '结果页',
-        path: '/pages/page-result/index',
-      },
-      {
-        name: 'Message',
-        nameZN: '信息状态、操作结果',
-        path: '/pages/message/index',
-      },
-      {
-        name: 'Pagination',
-        nameZN: '分页',
-        path: '/pages/pagination/index',
-      },
-      {
-        name: 'Title',
-        nameZN: '模块标题',
-        path: '/pages/title/index',
-      },
-      {
-        name: 'Container',
-        nameZN: '容器',
-        path: '/pages/container/index',
-      },
-      {
-        name: 'Form',
-        nameZN: '表单',
-        path: '/pages/form/index',
-      },
-      {
-        name: 'Mask',
-        nameZN: '背景蒙层',
-        path: '/pages/mask/mask',
-      },
-      {
-        name: 'Guide',
-        nameZN: '遮罩引导',
-        path: '/pages/guide/guide',
-      },
-      {
-        name: 'Avatar',
-        nameZN: '头像',
-        path: '/pages/avatar/index',
-      },
-      {
-        name: 'Loading',
-        nameZN: 'loading 加载',
-        path: '/pages/loading/loading',
-      },
-      {
-        name: 'Tag',
-        nameZN: ' 标签',
-        path: '/pages/tag/index',
-      },
-      {
-        name: 'Terms',
-        nameZN: ' 协议',
-        path: '/pages/terms/index',
-      },
-      {
-        name: 'SwipeAction',
-        nameZN: '滑动删除',
-        path: '/pages/swipe-action/index',
-      },
-      {
-        name: 'Collapse',
-        nameZN: '折叠',
-        path: '/pages/collapse/index',
-      },
-      {
-        name: 'Stepper',
-        nameZN: 'Stepper',
-        path: '/pages/stepper/index',
-      },
-      {
-        name: 'Calendar',
-        nameZN: '日历',
-        path: '/pages/calendar/index',
-      },
-      // {
-      //   name: 'FaceDetection',
-      //   nameZN: '（旧组件）人脸检测',
-      //   path: '/pages/face-detection/index',
-      // },
-      {
-        name: 'InputItem',
-        nameZN: '（旧组件）列表输入项',
-        path: '/pages/input-item/index',
-      },
-      {
-        name: 'PickerItem',
-        nameZN: '（旧组件）选择输入项',
-        path: '/pages/picker-item/index',
-      },
-      {
-        name: 'Flex',
-        nameZN: '（旧组件）Flex 布局',
-        path: '/pages/flex/index',
-      },
-      {
-        name: 'PopUp',
-        nameZN: '（旧组件）弹出菜单',
-        path: '/pages/popup/index',
+        components: [
+          {
+            type: '其他',
+            list: [
+              {
+                name: 'Pagination',
+                nameZN: '分页',
+                path: '/pages/pagination/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Alphabet',
+                nameZN: '字母检索表',
+                path: '/pages/alphabet/index',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+              {
+                name: 'Stepper',
+                nameZN: 'Stepper',
+                path: '/pages/stepper/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Calendar',
+                nameZN: '日历',
+                path: '/pages/calendar/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'AmIcon',
+                nameZN: '图标',
+                path: '/pages/am-icon/index',
+                tag: '优化',
+                tagType: 'success',
+              },
+              {
+                name: 'Loading',
+                nameZN: 'loading 加载',
+                path: '/pages/loading/loading',
+                tag: '新组件',
+                tagType: 'danger',
+              },
+            ],
+          },
+        ],
       },
     ],
+    // components: [
+    //   // {
+    //   //   name: 'Terms',
+    //   //   nameZN: 'Terms',
+    //   //   path: '/pages/terms/index',
+    //   // },
+    //   // {
+    //   //   name: 'FaceDetection',
+    //   //   nameZN: '（旧组件）人脸检测',
+    //   //   path: '/pages/face-detection/index',
+    //   // },
+    //   // {
+    //   //   name: 'InputItem',
+    //   //   nameZN: '（旧组件）列表输入项',
+    //   //   path: '/pages/input-item/index',
+    //   // },
+    //   // {
+    //   //   name: 'PickerItem',
+    //   //   nameZN: '（旧组件）选择输入项',
+    //   //   path: '/pages/picker-item/index',
+    //   // },
+    // ],
   },
 });
