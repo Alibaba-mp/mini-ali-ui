378b636e7cb5cf552e250d6b2b250a9b15ad40b0
小志 <yousai.lys@antfin.com>
2020-02-04 19:32:43 +0800

    修改 pub.js 的脚本,只有在 px 版本时才会提交到 git 仓库中

--- scripts/pub.js
+++ scripts/pub.js
@@ -8,20 +8,23 @@ const isRpx = process.argv.splice(2)[0] === '--rpx';
 if (isRpx) {
   packageJson.name = 'mini-ali-ui-rpx';
   packageJson.description = '小程序版AntUI rpx 版本';
+
+  const runner = spawn('npm', ['publish']);
+
+  runner.on('close', () => { });
 } else {
   packageJson.name = 'mini-ali-ui';
   packageJson.description = '小程序版AntUI';
+
+  const runner = spawn('npm', ['publish']);
+
+  runner.on('close', () => {
+    execSync(`git tag ${version}`);
+    execSync(`git push origin ${version}:${version}`);
+    execSync('git push origin master:master');
+  });
 }
 
 fs.writeFile(`${process.cwd()}/package.json`, JSON.stringify(packageJson), (err) => {
   if (err) console.error(err);
 });
-
-const runner = spawn('npm', ['publish']);
-
-runner.on('close', () => {
-  execSync(`git tag ${version}`);
-  execSync(`git push origin ${version}:${version}`);
-  execSync('git push origin master:master');
-});
-
