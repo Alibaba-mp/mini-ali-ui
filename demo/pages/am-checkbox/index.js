b8f1ccbefb6705e5d274f162ceae587a367eeb44
jiang.he <573748150jh@163.com>
2020-04-24 14:29:51 +0800

    fix(): 破坏性修改,需要跟大版本发布! 设计使用上对齐antd checkbox组件,解决#17问题

--- demo/pages/am-checkbox/index.js
+++ demo/pages/am-checkbox/index.js
@@ -1,9 +1,11 @@
 Page({
   data: {
     items: [
-      { checked: true, disabled: false, value: 'a', title: '复选框-默认选中', id: 'checkbox1' },
-      { checked: false, disabled: false, value: 'b', title: '复选框-默认未选中', id: 'checkbox2' },
-      { checked: true, disabled: true, value: 'c', title: '复选框-默认选中disabled', id: 'checkbox3' },
+      { defaultChecked: true, disabled: false, value: 'a', title: '复选框-默认选中', id: 'checkbox1' },
+      { defaultChecked: true, disabled: true, value: 'b', title: '复选框-默认选中disabled', id: 'checkbox2' },
+    ],
+    items1: [
+      { checked: false, disabled: false, value: 'c', title: '复选框-默认未选中', id: 'checkbox3' },
       { checked: false, disabled: true, value: 'd', title: '复选框-默认未选中disabled', id: 'checkbox4' },
     ],
   },
@@ -13,5 +15,36 @@ Page({
     });
   },
   onReset() {},
-  onChange() {},
+  onChange(e) {
+    const { id } = e.currentTarget.dataset;
+    const { value } = e.detail;
+    const { items1 } = this.data;
+    items1.forEach((element) => {
+      if (element.id === id) {
+        // eslint-disable-next-line no-param-reassign
+        element.checked = value;
+      }
+    });
+    this.setData(
+      items1,
+    );
+  },
+  // 全选
+  checkedON() {
+    this.checkedAll(true);
+  },
+  // 全不选
+  checkedOFF() {
+    this.checkedAll(false);
+  },
+  checkedAll(status) {
+    const { items1 } = this.data;
+    items1.forEach((element) => {
+      // eslint-disable-next-line no-param-reassign
+      element.checked = status;
+    });
+    this.setData(
+      items1,
+    );
+  },
 });
