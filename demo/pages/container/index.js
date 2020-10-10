ce142aa4c32540315822998008fa84a80f6ef8b8
linxz <linxz@vip.qq.com>
2020-10-10 16:29:26 +0800

    feat: container 组件增加 title 属性功能

--- demo/pages/container/index.js
+++ demo/pages/container/index.js
@@ -1,4 +1,10 @@
 Page({
   data: {},
   onLoad() {},
+  titleClick() {
+    my.alert({
+      title: 'onActionTap 回调',
+      content: '标题后面操作区域点击',
+    });
+  },
 });
--- src/container/index.ts
+++ src/container/index.ts
@@ -1,12 +1,27 @@
+import fmtEvent from '../_util/fmtEvent';
+
 Component({
   mixins: [],
   data: {},
   props: {
     className: '',
     type: 'line',
+    title: '',
+    thumb: '',
+    icon: '',
+    custom: '',
+    onActionTap: () => {},
   },
   didMount() {},
   didUpdate() {},
   didUnmount() {},
-  methods: {},
+  methods: {
+    onTitleClick(e) {
+      const { onActionTap } = this.props;
+      const event = fmtEvent(this.props, e);
+      if (typeof onActionTap === 'function') {
+        onActionTap(event);
+      }
+    },
+  },
 });
