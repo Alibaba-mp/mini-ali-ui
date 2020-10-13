1a0a73a8149cf8f4fa99b1d073080ba1adede96d
linxz <linxz@vip.qq.com>
2020-10-13 18:13:50 +0800

    fix: tabs 组件iOS样式兼容

--- src/tabs/index.ts
+++ src/tabs/index.ts
@@ -63,6 +63,7 @@ Component({
     autoplay: false,
     animation: false,
     showLeftShadow: false,
+    showRightShadow: true,
     version: my.SDKVersion,
     viewScrollLeft: 0,
     tabViewNum: 0,
@@ -276,13 +277,25 @@ Component({
       }
     },
     showLeftShadow(e) {
-      if (e.detail.scrollLeft > 0) {
+      const { scrollLeft, scrollWidth } = e.detail;
+      // 判断是否隐藏左边的阴影
+      if (scrollLeft > 0) {
         this.setData({
           showLeftShadow: true,
         });
       } else {
         this.setData({
           showLeftShadow: false,
+        });
+      }
+      // 判断是否隐藏右边的阴影
+      if (scrollLeft + this.data.boxWidth >= scrollWidth - 8) {
+        this.setData({
+          showRightShadow: false,
+        });
+      } else {
+        this.setData({
+          showRightShadow: true,
         });
       }
     },
