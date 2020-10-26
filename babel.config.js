c30e1425a2af8a200dc24bf6824e09ffc3c36358
linxz <linxz@vip.qq.com>
2020-10-26 19:18:09 +0800

    chore: 添加babel/runtime

--- babel.config.js
+++ babel.config.js
@@ -9,5 +9,8 @@ module.exports = {
     ],
     '@babel/preset-typescript',
   ],
-  plugins: ['@babel/plugin-syntax-dynamic-import'],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-runtime',
+  ],
 };
