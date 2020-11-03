b314e1919b4bf0b3aca63d4503f4a9f4aa962926
linxz <linxz@vip.qq.com>
2020-11-03 10:40:00 +0800

    fix: list 组件 worker render 的报错

--- src/list/index.ts
+++ src/list/index.ts
@@ -18,17 +18,21 @@ Component({
   didMount() {
     const loadTxt = this.props.loadContent[0] ? this.props.loadContent[0] : this.data.loadContent[0];
     const overTxt = this.props.loadContent[1] ? this.props.loadContent[1] : this.data.loadContent[1];
-    this.setData({
-      loadContent: [loadTxt, overTxt],
-    });
+    setTimeout(() => {
+      this.setData({
+        loadContent: [loadTxt, overTxt],
+      });
+    }, 0);
   },
   didUpdate() {
     const loadTxt = this.props.loadContent[0] ? this.props.loadContent[0] : this.data.loadContent[0];
     const overTxt = this.props.loadContent[1] ? this.props.loadContent[1] : this.data.loadContent[1];
     if (loadTxt !== this.data.loadContent[0] || overTxt !== this.data.loadContent[1]) {
-      this.setData({
-        loadContent: [loadTxt, overTxt],
-      });
+      setTimeout(() => {
+        this.setData({
+          loadContent: [loadTxt, overTxt],
+        });
+      }, 0);
     }
   },
 });
