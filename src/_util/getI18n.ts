54fa0a736a80fbe9565a86dd7f44cce50a691791
linxz <linxz@vip.qq.com>
2021-01-12 11:45:01 +0800

    fix: getI18n中增加对getApp()的判断

--- src/_util/getI18n.ts
+++ src/_util/getI18n.ts
@@ -2,16 +2,20 @@ import I18n_zhCN from '../_lang/zh-CN';
 import I18n_enUS from '../_lang/en-US';
 
 export default function getI18n() {
-  /* global getApp */
-  /* eslint no-undef: "error" */
-  const appMiniAliUI = getApp() || null;
-  if (appMiniAliUI) {
-    if (appMiniAliUI.globalData?.miniAliUiLang === 'en-US') {
-      return I18n_enUS;
+  try {
+    /* global getApp */
+    /* eslint no-undef: "error" */
+    const appMiniAliUI = getApp() || null;
+    if (appMiniAliUI) {
+      if (appMiniAliUI.globalData?.miniAliUiLang === 'en-US') {
+        return I18n_enUS;
+      } else {
+        return I18n_zhCN;
+      }
     } else {
       return I18n_zhCN;
     }
-  } else {
+  } catch (error) {
     return I18n_zhCN;
   }
 }
